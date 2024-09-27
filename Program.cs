11857127f53f19b33f5fe70c483fda01c21ce101
AmandaJustin <amandajustindelima13@gmail.com>
2024-09-26 21:39:22 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Retangulo R1 = new Retangulo();
+Retangulo R2 = new Retangulo(5);
+Retangulo R3 = new Retangulo(4, 6);
+
+Console.WriteLine($"A area do retangulo R1 = {R1.CalculaArea()}");
+Console.WriteLine($"O perimetro do retangulo R1 = {R1.CalcularPerimetro()}");
+
+Console.WriteLine($"A area do retangulo R2 = {R2.CalculaArea()}");
+Console.WriteLine($"O perimetro do retangulo R2 = {R2.CalcularPerimetro()}");
+
+Console.WriteLine($"A area do retangulo R3 = {R3.CalculaArea()}");
+Console.WriteLine($"O perimetro do retangulo R3 = {R3.CalcularPerimetro()}");
+
+R3.Redimensionar(2);
+Console.WriteLine($"A area do retangulo R3 = {R3.CalculaArea()}");
+Console.WriteLine($"O perimetro do retangulo R3 = {R3.CalcularPerimetro()}");
+
+R2.Redimensionar(10, 15);
+Console.WriteLine($"A area do retangulo R2 = {R2.CalculaArea()}");
+Console.WriteLine($"O perimetro do retangulo R2 = {R2.CalcularPerimetro()}");
